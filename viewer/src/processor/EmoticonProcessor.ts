45c7415b4e92a705e1abae7ab5c48324975dd6b4
skatpgusskat <skatpgusskat@naver.com>
2018-10-03 21:12:41 +0900

    Move viewer client to directory /viewer

--- viewer/src/processor/EmoticonProcessor.ts
+++ viewer/src/processor/EmoticonProcessor.ts
@@ -11,7 +11,7 @@ export default class Processor implements IProcessor {
     imageUrl: 'https://s3.ap-northeast-2.amazonaws.com/redchat-emoticon/%EC%9A%B0%EB%A6%AC%ED%95%B5%EC%95%84%EB%8B%8C%EB%8D%B0.gif',
   }];
   public process(chat: IChat): IChat {
-    const emoticon = this.emoticons.find((item) => chat.content.startsWith(item.key))
+    const emoticon = this.emoticons.find((item) => chat.content.startsWith(item.key));
     if (!emoticon) {
       return chat;
     }
